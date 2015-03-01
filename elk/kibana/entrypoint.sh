9b11464f3100ed201fba6f79270a3bcbb0e0dda4
gzoritchak <g.zoritchak@gmail.com>
2015-03-01 11:19:22 +0100

    Première version avec docker-compose.

---
+++ elk/kibana/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Wait for the Elasticsearch container to be ready before starting Kibana.
+echo "Stalling for Elasticsearch"
+#while true; do
+#    nc -q 1 elasticsearch 9200 >/dev/null && break
+#done
+
+sleep 10
+
+echo "Starting Kibana"
+/opt/kibana/bin/kibana
